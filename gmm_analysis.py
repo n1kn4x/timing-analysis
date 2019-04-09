e171152e5bf48846a860470399bb2d53f2059eca
Niklas Uli Pirnay <niklas.pirnay@campus.tu-berlin.de>
2019-04-09 22:12:49 +0200

    moved to password_timing_analysis linked with gmm analysis

---
+++ gmm_analysis.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+from sklearn.mixture import GaussianMixture
+from sklearn.mixture import BayesianGaussianMixture
+
+num_queries = None
+g = BayesianGaussianMixture(n_components=2,
+    weight_concentration_prior=np.finfo(np.longdouble).resolution*2,
+    warm_start=True)
+
+def flatten(data):
+    data_vec = data.values.flatten()
+    data_vec = data_vec[~np.isnan(data_vec)]
+    data_vec = data_vec.reshape(-1, 1)
+    return data_vec
+
+def max_gaussian(g):
+    return g.means_.argmax()
+
+def middle_gaussian(g):
+    a = pd.DataFrame([x[0] for x in g.means_])
+    idx = a[a==a.median()].dropna().index[0]
+    if len(a) % 2 == 0:
+        idx = int(idx-.5)
+    return idx
+
+def build_gmm(data, states):
+    data_vec = flatten(data)
+    g.fit(data_vec)
+    return g
+
+def get_gmm_predictions(data, g):
+    result = pd.DataFrame()
+    for char, obs in data.iteritems():
+        obs = flatten(obs)
+        if len(obs) > 0:
+            Y = g.predict(obs)
+            u, counts = np.unique(Y, return_counts=True)
+            result[char] = pd.Series(counts)
+    return result
+
+def get_gmm_prediction_ratios(data, g, idx):
+    predictions = get_gmm_predictions(data, g)
+    predictions[predictions.isnull()] = 0
+    def comp_ratio(counts):
+        S = np.sum(counts)
+        if S < num_queries*2/3:
+            return None
+        cc = counts[idx]
+        l1, l2 = cc, S
+        return l1 / l2
+    try:
+        ratios = predictions.apply(comp_ratio)
+    except:
+        print("Couldn't apply GMM successfully")
+        return None
+    return ratios
+
+def do_gmm_analysis(data, states=2):
+    data = data[data > 0]
+    g = build_gmm(data, states)
+    select_state = max_gaussian(g)
+    ratios = get_gmm_prediction_ratios(data, g, select_state)
+    return ratios
--- password_timing_analysis.py
+++ password_timing_analysis.py
@@ -4,19 +4,12 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import gmm_analysis as ga
 
 
 """
     fill_char must not be in alphabet.
 """
-
-def build_char_combinations(char_collection,
-                            combi_size):
-    assert(combi_size > 0)
-    char_combinations = itertools.product(char_collection,
-                                          repeat=combi_size)
-
-    return list(map(lambda chars: ''.join(chars), char_combinations))
 
 class Timea:
     # Default configuration
@@ -25,16 +18,15 @@ class Timea:
                 length,
                 prefix = "",
                 fill_char=":",
-                queries = 4,
-                char_collection = string.digits + string.ascii_letters,
-                combi_size = 1,
+                queries = 400,
+                char_collection = list(string.digits+string.ascii_letters),
                 decision_metric = "mean",
                 decision_rule = "max",
                 percentile = 3
                 ):
 
         assert(decision_metric in
-                ["mean","median","variance","percentile"])
+                ["gmm","mean","median","variance","percentile"])
         assert(decision_rule in
                 ["max", "min"])
         assert(len(fill_char) == 1)
@@ -44,37 +36,39 @@ class Timea:
         self.fill_char = fill_char
         self.queries = queries
         self.char_collection = char_collection
-        self.combi_size = combi_size
         self.decision_metric = decision_metric
         self.decision_rule = decision_rule
         self.percentile = percentile
 
-        self.char_collection = fill_char + char_collection
-        self.char_combis = build_char_combinations(self.char_collection,
-                                                        self.combi_size)
+        ga.num_queries = self.queries
+
+        self.char_collection = [fill_char] + char_collection
 
         if (self.decision_rule == "max"):
             self.decide = pd.Series.idxmax
         elif (self.decision_rule == "min"):
             self.decide = pd.Series.idxmin
-        if (self.decision_metric == "mean"):
-            self.analyze = np.nanmean
-        elif (self.decision_metric == "median"):
-            self.analyze = np.nanmedian
-        elif (self.decision_metric == "variance"):
-            self.analyze = np.nanvar
-        elif (self.decision_metric == "percentile"):
-            self.analyze = lambda x: np.nanpercentile(x, self.percentile)
 
+        if (self.decision_metric == "mean"):
+            self.analyze = pd.DataFrame.mean
+        elif (self.decision_metric == "gmm"):
+            self.analyze = ga.do_gmm_analysis
+        elif (self.decision_metric == "median"):
+            self.analyze = pd.DataFrame.median
+        elif (self.decision_metric == "variance"):
+            self.analyze = pd.DataFrame.var
+        elif (self.decision_metric == "percentile"):
+            p = lambda x: np.nanpercentile(x, self.percentile)
+            self.analyze = lambda x: pd.DataFrame.apply(x, p)
 
     def compose_password(self, prefix):
         filler = self.fill_char * (self.length - len(prefix))
         return prefix + filler
 
     def make_measurements(self, prefix=""):
-        all_candidates = [self.compose_password(prefix+char) for char in self.char_combis]
+        all_candidates = [self.compose_password(prefix+char) for char in self.char_collection]
         ref_candidate = self.compose_password(prefix)
-        result = pd.DataFrame(columns=self.char_combis)
+        result = pd.DataFrame(columns=self.char_collection)
         for k in range(self.queries):
             measurements = []
             for candidate in all_candidates:
@@ -87,8 +81,8 @@ class Timea:
     def sanitize_data(self, data):
         def outliers(col):
             median = col.median()
-            q1 = col.quantile(.10)
-            q3 = col.quantile(.90)
+            q1 = col.quantile(.1)
+            q3 = col.quantile(.9)
             iq_range = q3 - q1
             res = np.logical_or(col > (median + (1.5* iq_range)), \
                                 col < (median - (1.5* iq_range)))
@@ -100,23 +94,17 @@ class Timea:
         return data[~(outlier_mask)]
 
     def get_best_char(self, measurements):
-        if self.combi_size == 1:
-            del measurements[self.fill_char]
-        #measurements = measurements[measurements >= 0]
-        data = measurements.apply(self.analyze, axis=0)
-        decision = self.decide(data)
-        print(decision)
-        print(data[decision])
-        """
-        # DEBUG
-        plt.figure()
-        plt.hist(10**6*measurements[decision], 100, color='green',alpha=0.8)#, range=(-1.5, 1.5))
-        plt.axvline(x=data[decision]*10**6, linewidth=1, color='r')
-        plt.show()
-        # DEBUG
-        """
+        data = self.analyze(measurements)
+        if data is None:
+            return ""
+        try:
+            decision = self.decide(data)
+        except:
+            return ""
+        if self.decision_metric=="gmm" and data[decision] < 0.9:
+            return ""
+        print(decision, data[decision])
         return decision
-
 
     def run(self):
         prefix = self.prefix
@@ -126,4 +114,6 @@ class Timea:
             measurements = self.sanitize_data(measurements)
             next_char = self.get_best_char(measurements)
             prefix = prefix + next_char
+            if next_char == "":
+                prefix = prefix[:-1]
             print(prefix)
